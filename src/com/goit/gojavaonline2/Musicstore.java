07734dcc49a8c1d255bdaf6683dda29e137323a8
DenRib <denrib12@gmail.com>
2016-03-12 21:44:45 +0200

    Initial commit

---
+++ src/com/goit/gojavaonline2/Musicstore.java
@@ -0,0 +1,12 @@
+package com.goit.gojavaonline2;
+
+import java.util.List;
+
+/**
+ * Created by Денис Риб on 12.03.2016.
+ */
+public class Musicstore {
+    List<Guitar> guitars;
+    List<Piano> pianos;
+    List<Trumpet> trumpets;
+}
---
+++ src/com/goit/gojavaonline2/Trumpet.java
@@ -0,0 +1,7 @@
+package com.goit.gojavaonline2;
+
+/**
+ * Created by Денис Риб on 12.03.2016.
+ */
+public class Trumpet extends Musicinstrument {
+}
